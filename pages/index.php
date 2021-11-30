2e6b0d06971d372c188a661a75a716120dc5d8de
Angel Armando Saldivar <ang.arm.saldivar@gmail.com>
2021-11-30 09:55:01 -0600

    Commit 2 30/nov/2021

--- pages/index.php
+++ pages/index.php
@@ -335,10 +335,10 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong>Copyright &copy; 2021 <a href="https://adminlte.io">FIndme</a>.</strong>
+    Todos los derechos reservados
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0-rc
+      <b>Version</b> 3.0.0
     </div>
   </footer>
 </div>
